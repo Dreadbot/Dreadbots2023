5da3e215afcdd8625667529b492ef97fea5e4bd1
Odog0 <c3pooliver@gmail.com>
2023-01-14 14:06:49 -0500

    added first test of gryo balancing

--- robot/src/main/java/frc/robot/Robot.java
+++ robot/src/main/java/frc/robot/Robot.java
@@ -15,9 +15,22 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
+  public static final I2C.Port        I2C_PORT       = I2C.Port.kOnboard;
+  public static final SerialPort.Port GYROSCOPE_PORT = SerialPort.Port.kUSB;
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  //private CANSparkMax motor = new CANSparkMax(1, MotorType.kBrushless);
+  private DreadbotMotor motor = new DreadbotMotor(new CANSparkMax(1, MotorType.kBrushless), "Motor");
+  private DreadbotMotor motor3 = new DreadbotMotor(new CANSparkMax(3, MotorType.kBrushless), "Motor3");
+  private DreadbotMotor motor10 = new DreadbotMotor(new CANSparkMax(10, MotorType.kBrushless), "Motor10");
+  private DreadbotMotor motor2 = new DreadbotMotor(new CANSparkMax(2, MotorType.kBrushless), "Motor10");
+  private AHRS gyro = new AHRS(SerialPort.Port.kMXP);
+  private double pitch = 0;
+  private double yaw = 0;
+  private double roll = 0;
+  private DreadbotController controller = new DreadbotController(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -81,7 +94,121 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    motor.set(controller.getYAxis());
+    motor10.set(controller.getYAxis());
+    motor3.set(controller.getYAxis());
+    motor2.set(controller.getYAxis());
+    double maxspeed = 0.4;
+   /* System.out.println("X axis" + controller.getXAxis());
+    System.out.println("Y axis" + controller.getYAxis());*/
+
+    if (controller.getXAxis() > .1) {
+      motor.set(controller.getXAxis());
+      motor10.set(-controller.getXAxis());
+      motor3.set(controller.getXAxis());
+      motor2.set(-controller.getXAxis());
+      if (controller.getXAxis() > maxspeed) {
+        motor.set(maxspeed);
+        motor10.set(-maxspeed);
+        motor3.set(maxspeed);
+        motor2.set(-maxspeed);
+      }
+    } else if (controller.getXAxis() < -.1) {
+      motor.set(controller.getXAxis());
+      motor10.set(-controller.getXAxis());
+      motor3.set(controller.getXAxis());
+      motor2.set(-controller.getXAxis());
+        if (controller.getXAxis() < -maxspeed) {
+          motor.set(-maxspeed);
+          motor10.set(maxspeed);
+          motor3.set(-maxspeed);
+          motor2.set(maxspeed);
+        } 
+    } else if (controller.getYAxis() > maxspeed) {
+      motor.set(maxspeed);
+      motor10.set(maxspeed);
+      motor3.set(maxspeed);
+      motor2.set(maxspeed);
+    } else if (controller.getYAxis() < -maxspeed) {
+      motor.set(-maxspeed);
+      motor10.set(-maxspeed);
+      motor3.set(-maxspeed);
+      motor2.set(-maxspeed);
+    } 
+    if (controller.isXButtonPressed()){
+      pitch = gyro.getPitch();
+      // System.out.println("Pitch" + pitch);
+       yaw = gyro.getYaw();
+       //System.out.println("Yaw" + yaw);
+       roll = gyro.getRoll();
+       System.out.println("Roll" + roll + 
+       "  Yaw" + yaw + "  Pitch" + pitch);
+       
+       double scale = .008;
+       double speed = 0;
+   
+       speed = pitch * scale;
+
+       if (speed > 0.1) {
+         speed = 0.1;
+       }
+       if (speed < -0.1){
+         speed = -0.1;
+       }
+
+       if(Math.abs(pitch)<2){speed=0;}
+   
+       motor.set(speed);
+       motor10.set(speed);
+       motor3.set(speed);
+       motor2.set(speed);
+   
+       SmartDashboard.putNumber("Pitch", pitch);
+       SmartDashboard.putNumber("Yaw", yaw);
+       SmartDashboard.putNumber("Roll", roll);
+       SmartDashboard.putNumber("Speed", speed);
+    }    
+    
+
+   /* if (pitch > 5) {
+    motor.set(.1);
+    motor10.set(.1);
+    motor3.set(.1);
+    motor2.set(.1);
+    }
+    if (pitch < -5) {
+    motor.set(-.1);
+    motor10.set(-.1);
+    motor3.set(-.1);
+    motor2.set(-.1);
+    }
+    */
+
+    /*
+    if(controller.isDpadUpPressed()) {
+      motor.set(.2);
+      motor3.set(.2);
+      motor10.set(.2);
+    } else if(controller.isDpadDownPressed()) {
+      motor.set(-.2);
+      motor3.set(-.2);
+      motor10.set(-.2);
+    } else if(controller.isDpadUpPressed()) {
+      motor.set(-.2);
+      motor3.set(.2);
+      motor10.set(-.2);
+    } else if(controller.isLeftTriggerPressed()) {
+      motor.set(.2);
+      motor3.set(-.2);
+      motor10.set(.2);
+    } else {
+      motor.set(0);
+      motor3.set(0);
+      motor10.set(0);
+    } 
+    */
+  }
 
   @Override
   public void testInit() {
