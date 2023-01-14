2442ecf558375f864bbe1d527684958e37c9c7bf
MartinoPavesi <martinoepavesi@gmail.com>
2023-01-14 14:23:41 -0500

    added utils + fixed errors

--- robot/src/main/java/frc/robot/Robot.java
+++ robot/src/main/java/frc/robot/Robot.java
@@ -4,9 +4,18 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.util.controls.DreadbotController;
+import frc.robot.util.misc.DreadbotMotor;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
