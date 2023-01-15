6dc9af2e80b1db6f3a0ecc7d254544bd20ee1dc6
KCsup <kcsup05@gmail.com>
2023-01-14 21:33:55 -0500

    Added code from the demo.

--- vision/main.py
+++ vision/main.py
@@ -2,23 +2,48 @@ import cv2
 from dt_apriltags import Detector
 
 cap = cv2.VideoCapture(0)
-_, frame = cap.read()
-image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-cap.release()
-
-# imagepath = 'frame.png'
-# image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-
 at_detector = Detector(searchpath=['apriltags'],
-        families='tag16h5',
-        nthreads=2,
-        quad_sigma=0.2,
-        decode_sharpening=0.25,
-    )
+            families='tag16h5',
+            nthreads=2,
+            quad_sigma=0.2,
+            decode_sharpening=0.25,
+        )
 
-# with camera LC3
-camera_params = [660.1986, 660.2282, 314.2296, 235.1862]; # fx, fy, cx, cy
+while True:
+    _, frame = cap.read()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-tags = at_detector.detect(image, estimate_tag_pose=True, camera_params=camera_params, tag_size=0.1397)
-print(tags)
+    # imagepath = 'frame.png'
+    # image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+
+    # with camera LC3
+    camera_params = [660.1986, 660.2282, 314.2296, 235.1862]; # fx, fy, cx, cy
+
+    tags = at_detector.detect(image, estimate_tag_pose=True, camera_params=camera_params, tag_size=0.1397)
+    for tag in tags:
+        for c in range(4):
+            toI = c + 1
+            if(c == 3): toI = 0
+
+            original = tag.corners[c]
+            to = tag.corners[toI]
+            
+            cv2.line(frame, (int(original[0]), int(original[1])), (int(to[0]), int(to[1])), (0, 255, 0), 2)
+
+        center = tag.center
+        centerCord = (int(center[0]), int(center[1]))
+        cv2.putText(frame, "dZ: " + str(tag.pose_t[2]), centerCord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+
+    # print(tags)
+    cv2.imshow("frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+
+
 
