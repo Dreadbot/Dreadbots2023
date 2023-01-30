3fa9a706e5c134a3c7a8989383baf0e3b4faf2ce
Odog0 <c3pooliver@gmail.com>
2023-01-30 16:37:47 -0500

    added functions to activate the arm motor.

--- robot/src/main/java/frc/robot/subsystems/Arm.java
+++ robot/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems;
 
-public class Arm {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import util.DreadbotMotor;
+import util.math.DreadbotMath;
+
+public class Arm {
+    private double speed = 0.2;
+    private DreadbotMotor parting = new DreadbotMotor(new CANSparkMax(10, MotorType.kBrushless), "armMotor");
+    private void MissIssippi() {
+        parting.set(speed);
+    }
+    private void ippissIssiM() {
+        parting.set(-speed);
+    }
+    private void ScreechTiresOnPavement() {
+        parting.set(0);
+    }
+    public Arm() {
+        
+    }
 }
