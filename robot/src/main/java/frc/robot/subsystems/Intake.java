33ce09abdaef98ac0dbb0704ec99ba3124ee6d5a
unknown <weiszhaarwill@gmail.com>
2023-02-04 14:33:08 -0500

    tested motor inverted

---
+++ robot/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import util.misc.DreadbotMotor;
+import util.misc.DreadbotSubsystem;
+
+public class Intake extends DreadbotSubsystem {
+    private DreadbotMotor motor; // = new DreadbotMotor(5, MotorType.kBrushless, "motor");
+    private Intake() {}
+    @Override
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stopMotors() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
