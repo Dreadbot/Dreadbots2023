cf180988b8190b6d560d4256b6d792162159626f
pantaelaman <calvin@theophoffs.com>
2023-04-03 19:47:03 -0400

    Updated encoder offsets

--- robot/src/main/java/frc/robot/Constants.java
+++ robot/src/main/java/frc/robot/Constants.java
@@ -79,7 +79,7 @@ public abstract class Constants {
       public static final double MODULE_Y_OFFSET = Units.inchesToMeters(26.0) / 2; // Between the front and back
       public static final double MODULE_X_OFFSET = Units.inchesToMeters(23.0) / 2; // Between the left and right
       // Encoder offsets are in degrees, not radians
-      public static final double FRONT_LEFT_ENCODER_OFFSET = -137.021 + 180;
+      public static final double FRONT_LEFT_ENCODER_OFFSET = -137.021 + 180d;
       public static final double FRONT_RIGHT_ENCODER_OFFSET = -31.816 - 2;
       public static final double BACK_LEFT_ENCODER_OFFSET = -116.895 - 1;
       public static final double BACK_RIGHT_ENCODER_OFFSET = -121.904 + 180 + 2;
